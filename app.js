4322231bea7c4f8e30e0d6dbd13aaab483d75ca8
DylanLangevin <dylan.lgvn@gmail.com>
2022-04-15 00:08:30 +0200

    API en place, code clean

--- app.js
+++ app.js
@@ -1,17 +1,33 @@
+// API que j'utilise
 // https://the-trivia-api.com/docs/
 
+//  catégorie dans l'api
+// https://the-trivia-api.com/categories
+
+// Récupération des balises HTML
+const mainDiv = document.getElementById("main")
+const roundNumber = document.getElementById("round")
+const questionDiv = document.getElementById("question")
+
+const answer1 = document.getElementById("answer-1")
+const answer2 = document.getElementById("answer-2")
+const answer3 = document.getElementById("answer-3")
+
+const label1 = document.getElementById("label-1")
+const label2 = document.getElementById("label-2")
+const label3 = document.getElementById("label-3")
+
+const validateBtn = document.getElementById('validate-btn')
+
+// Déclaration des variables
 let category;
 let correctAnswer;
 let question;
 let incorrectAnswers = [];
 let allAnswers = [];
+let score = 1
 
-
-const mainDiv = document.getElementById("main")
-
-//  catégorie dans l'api
-// https://the-trivia-api.com/categories
-
+// Requête fetch de l'API
 function fetchQuestion(){
 
     fetch("https://the-trivia-api.com/questions?categories=arts_and_literature,film_and_tv,food_and_drink,general_knowledge,geography,history,music,science,society_and_culture,sport_and_leisure&limit=1")
@@ -21,128 +37,70 @@ function fetchQuestion(){
         fetchQuestionComplet(quiz)
         createQuestionDiv()
     })
-
-
 }
 
+// Récupération des propiétés de l'objet
 function fetchQuestionComplet(quiz) {
 
+    // Réinitialisation de ces listes
     incorrectAnswers = [];
-    allAnswers = []
+    allAnswers = [];
 
+    // Attribution des propriétés de l'objet dans des variables pour les utiliser
     category = quiz[0].category
     correctAnswer = quiz[0].correctAnswer
     question = quiz[0].question
 
+    // Injection des mauvaises réponses d'abord
     for (let i = 0; i<2; i++){
-
         incorrectAnswers.push(quiz[0].incorrectAnswers[i])
     }
+
+    // Puis injection de la bonne réponse
     allAnswers = incorrectAnswers
-   
     allAnswers.push(correctAnswer)
 
-
-    
-
-
-    // Melanger la liste de réponse
+    // Melange la liste de réponses
     allAnswers.sort()
-    console.log(allAnswers);
-    console.log(correctAnswer);
-    
-
-
-
 }
 
-
-
-
-
-let score = 1
-let array = []
-// Base de données de quiz en attendant d'utiliser l'API
-
-
-// Creation du HTML
-const questionsDiv = document.createElement("div");
-
+// Injection du contenu dans les balises
 function createQuestionDiv() {
     
+    // Injection du score
+    roundNumber.textContent = "Manche " + score;
 
+    // Injection de la question
+    questionDiv.textContent = String(question);
 
-    console.log("score :",score);
-    questionsDiv.innerHTML = `
-    <h4> Manche ${score} </h4>
-    <form id="form-quiz">
-        <div class="question-container">
+    // Injection des réponses
+    answer1.value = String(allAnswers[0])
+    label1.textContent = String(allAnswers[0])
 
-            <h4 class="question">${question}</h4>
+    answer2.value = String(allAnswers[1])
+    label2.textContent = String(allAnswers[1])
 
-            <div class="answer-container">
-
-                    <input type="radio" id="answer" value="${allAnswers[0]}" name="question-1" checked>
-                    <label for="answer1">${allAnswers[0]}</label><br>
-
-                    <input type="radio" id="answer" value="${allAnswers[1]}" name="question-1">
-                    <label for="answer2">${allAnswers[1]}</label><br>
-
-                    <input type="radio" id="answer" value="${allAnswers[2]}" name="question-1">
-                    <label for="answer3">${allAnswers[2]}</label><br>
-                    
-            </div>
-        </div>
-    </form>
-
-    `
-    mainDiv.appendChild(questionsDiv)
-    
-    
+    answer3.value = String(allAnswers[2])
+    label3.textContent = String(allAnswers[2])
 }
-
 
 fetchQuestion()
 
-// Déclaration de variable après création du HTML
-const validateBtn = document.getElementById('validate-btn')
-const form = document.getElementById('form-quiz')
-
-
+// Au clique du bouton
 validateBtn.onclick = function(e){
-    // Récupération du radio button checked
+    // Récupération de la valeur du bouton radio coché
     const radioButtonsChecked = document.querySelector('input[name="question-1"]:checked').value;
 
-    console.log("notre réponse", radioButtonsChecked);
-    // e.preventDefault();
-
-    // condition si la réponse est bonne
+    // Entrée de la condition : si la valeur du bouton radio coché est égale à la bonne réponse
     if (radioButtonsChecked == correctAnswer) {
 
-        // Passer a la manche supérieure
-        score+=1
-        // array.push(true)
+        // Le score est incrémenté de 1, et on effectue une nouvelle requête
+        score += 1
         fetchQuestion()
-        console.log("Bonne réponse");
         
     } else {
 
-
-        console.log("Mauvaise réponse");
+        // Rafraichissement de la page
         location.reload();
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
