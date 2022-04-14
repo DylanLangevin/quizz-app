2cb39f296b56ea0ef92f4c005ed394fc5fcbc6a0
DylanLangevin <dylan.lgvn@gmail.com>
2022-04-14 23:15:10 +0200

    API en place

--- app.js
+++ app.js
@@ -1,61 +1,95 @@
-// TROUVER UN MOYEN DE METTRE A JOUR LE SCORE A CHAQUE REP
+// https://the-trivia-api.com/docs/
+
+let category;
+let correctAnswer;
+let question;
+let incorrectAnswers = [];
+let allAnswers = [];
+
+
 const mainDiv = document.getElementById("main")
+
+//  catégorie dans l'api
+// https://the-trivia-api.com/categories
+
+function fetchQuestion(){
+
+    fetch("https://the-trivia-api.com/questions?categories=arts_and_literature,film_and_tv,food_and_drink,general_knowledge,geography,history,music,science,society_and_culture,sport_and_leisure&limit=1")
+    .then(reponse => reponse.json())
+    .then((quiz) => {
+        console.log(quiz);
+        fetchQuestionComplet(quiz)
+        createQuestionDiv()
+    })
+
+
+}
+
+function fetchQuestionComplet(quiz) {
+
+    incorrectAnswers = [];
+    allAnswers = []
+
+    category = quiz[0].category
+    correctAnswer = quiz[0].correctAnswer
+    question = quiz[0].question
+
+    for (let i = 0; i<2; i++){
+
+        incorrectAnswers.push(quiz[0].incorrectAnswers[i])
+    }
+    allAnswers = incorrectAnswers
+   
+    allAnswers.push(correctAnswer)
+
+
+    
+
+
+    // Melanger la liste de réponse
+    allAnswers.sort()
+    console.log(allAnswers);
+    console.log(correctAnswer);
+    
+
+
+
+}
+
+
+
+
+
 let score = 1
 let array = []
 // Base de données de quiz en attendant d'utiliser l'API
-let questions = [
 
-    "Qui est l'empereur de France le 2 décembre 1804 ?",
-    "Quelle est la date d'indépendance des Etats-Unis ?",
-    "La chute de l'empire Romain se situe en ?",
-    "Quelle est la capitale de la Slovénie ?",
-    "Combien d'habitants compte l'Irlande en 2020 ?"
-]
-
-let allAnswers = [
-
-    ["Clovis","Abraham Lincoln","Napoleon Bonaparte"],
-    ["4 juillet 1776","18 avril 1856","30 juin 1925"],
-    ["15 ap. J.-C","395 ap. J.-C","-740 av. J.-C"],
-    ["Ljubljana","Belgrade","Bratislava"],
-    ["1,365 Million","21 Millions","4,9 Millions"],
-]
-
-let allCorrectAnswers = [
-
-    "Napoleon Bonaparte",
-    "4 juillet 1776",
-    "395 ap. J.-C",
-    "Ljubljana",
-    "4,9 Millions"
-]
-
-let randomNumber = Math.floor(Math.random() * questions.length)
 
 // Creation du HTML
 const questionsDiv = document.createElement("div");
 
-function createQuestionDiv(randomNumber) {
+function createQuestionDiv() {
+    
 
-    console.log("good",allCorrectAnswers[randomNumber]);
+
     console.log("score :",score);
     questionsDiv.innerHTML = `
     <h4> Manche ${score} </h4>
     <form id="form-quiz">
         <div class="question-container">
 
-            <h4 class="question">${questions[randomNumber]}</h4>
+            <h4 class="question">${question}</h4>
 
             <div class="answer-container">
 
-                    <input type="radio" id="answer" value="${allAnswers[randomNumber][0]}" name="question-1" checked>
-                    <label for="answer1">${allAnswers[randomNumber][0]}</label><br>
+                    <input type="radio" id="answer" value="${allAnswers[0]}" name="question-1" checked>
+                    <label for="answer1">${allAnswers[0]}</label><br>
 
-                    <input type="radio" id="answer" value="${allAnswers[randomNumber][1]}" name="question-1">
-                    <label for="answer2">${allAnswers[randomNumber][1]}</label><br>
+                    <input type="radio" id="answer" value="${allAnswers[1]}" name="question-1">
+                    <label for="answer2">${allAnswers[1]}</label><br>
 
-                    <input type="radio" id="answer" value="${allAnswers[randomNumber][2]}" name="question-1">
-                    <label for="answer3">${allAnswers[randomNumber][2]}</label><br>
+                    <input type="radio" id="answer" value="${allAnswers[2]}" name="question-1">
+                    <label for="answer3">${allAnswers[2]}</label><br>
                     
             </div>
         </div>
@@ -67,7 +101,8 @@ function createQuestionDiv(randomNumber) {
     
 }
 
-createQuestionDiv(randomNumber)
+
+fetchQuestion()
 
 // Déclaration de variable après création du HTML
 const validateBtn = document.getElementById('validate-btn')
@@ -81,17 +116,13 @@ validateBtn.onclick = function(e){
     console.log("notre réponse", radioButtonsChecked);
     // e.preventDefault();
 
-    console.log(randomNumber);
-
     // condition si la réponse est bonne
-    if (radioButtonsChecked == allCorrectAnswers[randomNumber]) {
+    if (radioButtonsChecked == correctAnswer) {
 
         // Passer a la manche supérieure
         score+=1
         // array.push(true)
-        randomNumber = Math.floor(Math.random() * questions.length)
-        createQuestionDiv(randomNumber)
-
+        fetchQuestion()
         console.log("Bonne réponse");
         
     } else {
